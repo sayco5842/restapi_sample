2e74732e609fa87a8bf296cf2418de98134730a1
정석 <sjung@deali.net>
2019-11-03 21:51:28 +0900

    first commit

---
+++ src/test/java/me/rido/demorestapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rido.demorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
